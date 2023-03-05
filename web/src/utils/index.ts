c6e7773523196119143c6d3648b6f2ac1ca6e42c
bangal18 <74682095+bangal18@users.noreply.github.com>
2023-03-05 01:43:00 +0100

    utils index

---
+++ web/src/utils/index.ts
@@ -0,0 +1,9 @@
+export { barConfiguration } from './bar_configuration'
+export { pieConfiguration } from './pie_configuration'
+export { sentimentColor, sentimentTextColor } from './sentiment_colors'
+export { politicalPartiesColor } from './political_parties_color'
+
+export type Label = string;
+export type Value = number;
+export type Color = string;
+export type BorderColor = string;
