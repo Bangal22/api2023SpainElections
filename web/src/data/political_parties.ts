0f539992fa8203c10c0965c07b456e4b23f41469
bangal18 <74682095+bangal18@users.noreply.github.com>
2023-03-05 01:44:37 +0100

    static data of political parties

---
+++ web/src/data/political_parties.ts
@@ -0,0 +1,36 @@
+import type { PoliticalParties } from "@model/index";
+
+export const politicalParties : PoliticalParties[] = [
+    {
+        name: "PSOE",
+        title: "Partido socialista obreo español",
+        png: "assets/psoe.png",
+        height: 100,
+        width: 100,
+        link: "https://www.psoe.es",
+    },
+    {
+        name: "PP",
+        title: "Partido popular",
+        png: "assets/pp.png",
+        height: 100,
+        width: 100,
+        link: "https://www.pp.es/",
+    },
+    {
+        name: "VOX",
+        title: "Vox",
+        png: "assets/vox.png",
+        height: 100,
+        width: 100,
+        link: "https://www.voxespana.es/",
+    },
+    {
+        name: "Unidas Podemos",
+        title: "Unidas podemos",
+        png: "assets/podemos.png",
+        height: 100,
+        width: 100,
+        link: "https://podemos.info/",
+    },
+];
