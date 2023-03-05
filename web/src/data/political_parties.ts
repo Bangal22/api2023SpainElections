a057e4fc03d80bb6fcc8576a61efc946683981c6
bangal18 <74682095+bangal18@users.noreply.github.com>
2023-03-05 22:23:04 +0100

    get_info service done

--- web/src/data/political_parties.ts
+++ web/src/data/political_parties.ts
@@ -2,7 +2,7 @@ import type { PoliticalParties } from "@model/index";
 
 export const politicalParties : PoliticalParties[] = [
     {
-        name: "PSOE",
+        name: "psoe",
         title: "Partido socialista obreo español",
         png: "assets/psoe.png",
         height: 100,
@@ -10,7 +10,7 @@ export const politicalParties : PoliticalParties[] = [
         link: "https://www.psoe.es",
     },
     {
-        name: "PP",
+        name: "pp",
         title: "Partido popular",
         png: "assets/pp.png",
         height: 100,
@@ -18,7 +18,7 @@ export const politicalParties : PoliticalParties[] = [
         link: "https://www.pp.es/",
     },
     {
-        name: "VOX",
+        name: "vox",
         title: "Vox",
         png: "assets/vox.png",
         height: 100,
@@ -26,11 +26,19 @@ export const politicalParties : PoliticalParties[] = [
         link: "https://www.voxespana.es/",
     },
     {
-        name: "Unidas Podemos",
+        name: "unidas podemos",
         title: "Unidas podemos",
         png: "assets/podemos.png",
         height: 100,
         width: 100,
         link: "https://podemos.info/",
     },
+    // {
+    //     name: "other",
+    //     title: "Other",
+    //     png: "assets/podemos.png",
+    //     height: 100,
+    //     width: 100,
+    //     link: "https://podemos.info/",
+    // },
 ];
--- web/src/service/get_info_by_political_parties.ts
+++ web/src/service/get_info_by_political_parties.ts
@@ -1,8 +1,8 @@
 import type { InfoByPoliticalParties } from '@model/index'
 import { variables } from 'src/variables'
 
-export async function getInfoByPoliticalParties(name: string): Promise<InfoByPoliticalParties> {
-    const response = await fetch(`${variables.API}/get-info?name=${name}`)
-    const data : InfoByPoliticalParties = await response.json()
+export async function getInfoByPoliticalParties(): Promise<InfoByPoliticalParties[]> {
+    const response = await fetch(`${variables.API}/get-info`)
+    const data : InfoByPoliticalParties[] = await response.json()
     return data
 }
