e2ea82d234c31108ee637bcaf238b7058434302f
bangal18 <74682095+bangal18@users.noreply.github.com>
2023-03-05 01:42:42 +0100

    default sentiment color file done

---
+++ web/src/utils/sentiment_colors.ts
@@ -0,0 +1,12 @@
+export const sentimentColor: any = {
+    negative: '#118DFF',
+    neutral: '#12239E',
+    positive: '#E66C37'
+}
+
+export const sentimentTextColor : any = {
+    negative: 'red',
+    neutral: 'black',
+    positive: '#49CC17'
+}
+// ["#E66C37", "#12239E", "#118DFF"]
