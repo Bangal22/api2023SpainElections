367abfd550873ddffd7020332b0ec00f5372cb3d
bangal18 <74682095+bangal18@users.noreply.github.com>
2023-03-05 01:42:06 +0100

    political parties color file done (default colors)

---
+++ web/src/utils/political_parties_color.ts
@@ -0,0 +1,22 @@
+export const politicalPartiesColor: any = {
+    'psoe': {
+        color: '#FD7F7D',
+        border_color: '#EA0300'
+    },
+    'pp': {
+        color: '#64A0FA',
+        border_color: '#1D498B'
+    },
+    'vox': {
+        color: '#99f776',
+        border_color: '#57BA33'
+    },
+    'unidas podemos': {
+        color: '#82558F',
+        border_color: '#663276'
+    },
+    'other': {
+        color: '#CDCDCD',
+        border_color: '#B3B3B3'
+    }
+}
